b2e2dff145fffbd06ef5156e0d7032f261b2a9b1
Mateusz Kolata <mkolata13@gmail.com>
2025-01-24 00:14:08 +0100

    szybki update

--- producer/src/main/java/kafka/Producer.java
+++ producer/src/main/java/kafka/Producer.java
@@ -38,9 +38,7 @@ public class Producer {
         producerConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         producerConfig.put(ProducerConfig.CLIENT_ID_CONFIG, "local");
         producerConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:9192, kafka2:9292, kafka3:9392");
-        producerConfig.put(ProducerConfig.ACKS_CONFIG, "all");
         producerConfig.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "4b05a509-d98d-4fa4-9daf-a8d329beb956");
-        producerConfig.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
 
         producer = new KafkaProducer<>(producerConfig);
         producer.initTransactions();
